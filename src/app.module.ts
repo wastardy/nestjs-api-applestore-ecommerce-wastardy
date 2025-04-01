d9f45a8a94ac78dd1449b4978f712b35f2a51c88
Andrew Kononenko <wastardy.k@gmail.com>
2025-04-01 17:49:48 +0300

    feat: add UserModule to the application for user management

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import envConfig from 'config/env.config';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
       load: [envConfig],
     }),
+    UserModule,
   ],
 })
 export class AppModule {}
