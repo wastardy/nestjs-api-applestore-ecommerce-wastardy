786a09cd9f6c4e9873fb1dd14ffc263f4a685996
Andrew Kononenko <wastardy.k@gmail.com>
2025-04-01 17:23:47 +0300

    feat: add environment configuration with validation using zod

---
+++ config/env.config.ts
@@ -0,0 +1,24 @@
+import { registerAs } from '@nestjs/config';
+import { z as zod } from 'zod';
+
+const envSchema = zod.object({
+  HOST: zod.string().default('localhost'),
+  PORT: zod.coerce.number().optional().default(3000),
+
+  BASE_URL: zod.string().optional().default('http://localhost:3000'),
+
+  DB_PORT: zod.number().optional().default(5432),
+  DB_HOST: zod.string().default('localhost'),
+  DB_USER: zod.string().default('postgres'),
+  DB_PASSWORD: zod.string().default('postgres'),
+  DB_NAME: zod.string().default('sasscribtion'),
+
+  DATABASE_URL: zod.string().optional(),
+
+  JWT_SECRET_KEY: zod.string().default('iST@P_O7{00x3_!tZZsJy'),
+  JWT_EXPIRES_IN: zod.string().default('1h'),
+});
+
+const parseEnv = envSchema.parse(process.env);
+
+export default registerAs('env', () => parseEnv);
