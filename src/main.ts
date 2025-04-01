d6cec8bdcfe2d29ee0b710ab2233635bb658bade
Andrew Kononenko <wastardy.k@gmail.com>
2025-04-01 17:24:19 +0300

    feat: enhance server startup to use HOST and PORT from configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const configService = app.get(ConfigService);
+  const host = configService.get<string>('HOST', 'localhost');
+  const port = configService.get<number>('PORT', 3000);
+
+  await app.listen(port, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+  });
 }
-bootstrap();
+
+void bootstrap();
