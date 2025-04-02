865d2ab11da1a5147b1501227a27ce30cc0f6c8f
Andrew Kononenko <wastardy.k@gmail.com>
2025-04-02 14:59:55 +0300

    feat: add global validation pipe to enforce DTO validation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
 
   const configService = app.get(ConfigService);
   const host = configService.get<string>('HOST', 'localhost');
