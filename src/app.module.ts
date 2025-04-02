86b78b8780ede9e99cf05b03f9e4afc8407525a1
Andrew Kononenko <wastardy.k@gmail.com>
2025-04-02 14:11:47 +0300

    feat: import PrismaModule in AppModule for database integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import envConfig from 'config/env.config';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 import { ProductModule } from './product/product.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { ProductModule } from './product/product.module';
     }),
     UserModule,
     ProductModule,
+    PrismaModule,
   ],
 })
 export class AppModule {}
