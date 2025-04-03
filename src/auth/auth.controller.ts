97675ca7159e5d8be803e9acf95795ddd35c1b16
Andrew Kononenko <wastardy.k@gmail.com>
2025-04-03 12:22:53 +0300

    feat: add signup endpoint to AuthController for user registration

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { SignupDto } from './dtos/signup.dto';
 
-@Controller({})
+@Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('signup')
+  signup(@Body() signupDto: SignupDto) {
+    return this.authService.signup(signupDto);
+  }
 }
